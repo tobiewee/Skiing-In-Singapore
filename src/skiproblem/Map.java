3ae57b602b9f36091a2db5dc63f21cbbfc7fbaf2
Tobie Wee <tobiewee@gmail.com>
2016-02-02 00:28:42 +0800

    Temporarily comment out ArrayList codes

--- src/skiproblem/Map.java
+++ src/skiproblem/Map.java
@@ -6,13 +6,13 @@ public class Map {
 	private int width;
 	private int height;
 	private int length;
-	private ArrayList map;
+	//private ArrayList map;
 	
 	public Map(){
 		width = 1;
 		height = 1;
 		length = 1;
-		map = new ArrayList<ArrayList<Integer>>();
+		//map = new ArrayList<ArrayList<Integer>>();
 	}
 	
 	public Map(int w, int h){
